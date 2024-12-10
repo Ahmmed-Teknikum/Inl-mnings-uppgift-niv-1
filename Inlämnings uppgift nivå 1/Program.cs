0fde35456eb4d0d8d20006028f67ec99b795247f
ahka07001 <ahka07001@E-PW05CGQT>
2024-12-10 14:22:05 +0100

    Add project files.

---
+++ Inlämnings uppgift nivå 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace inlämnings_._uppgift_._nivå_._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int x=1;x<11;x++)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
